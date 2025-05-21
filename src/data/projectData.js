352952695aee19d31fc52d758bab62ee11c13b31
Quang <nguyennhatquangkod2003@gmail.com>
2025-05-21 17:42:58 +0700

    feat: update archive

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,12 +2,12 @@ const projectData = [
     {
         id: "el",
         icon: "/portfolio/projects/ico_el.png",
-        title: "Elemental Labyrinth",
-        description: "Tower Defense Game with Mazing, Tower Fusion and Online Multiplayer",
+        title: "Elemental Labyrinth (Unity/C#)",
+        description: "A Tower Defense Game designed and developed by myself.",
         image: "projects/Elemental Labyrinth.png",
-        keyFeatures: [],
+        keyFeatures: ["Rouguelike", "Tower Defense", "P2P Multiplayer", "Steamwork Integration"],
         developmentTeam: "Solo Project",
-        technology: [],
+        technology: ["Unity", "C#", "Photoshop", "Asesprite", "FMod"],
 
         buttons: [
             {
@@ -27,12 +27,12 @@ const projectData = [
     {
         id: "wob",
         icon: "/portfolio/projects/ico_wob.png",
-        title: "World of Blast",
+        title: "World of Blast (React/PHP/Solidity)",
         description: "NFT Game on Blast Chain",
         image: "projects/wob.png",
-        keyFeatures: [],
-        developmentTeam: "5 People",
-        technology: [],
+        keyFeatures: ["NFT Integration", "Web3", "Play to Earn", "Wallet Authentication", "True Randomness", "Cached NFT Storage"],
+        developmentTeam: "5 Members",
+        technology: ["React", "Node.js", "Solidity", "Hardhat", "TypeScript", "Tailwind CSS", "PHP", "MySQL"],
         buttons: [
             {
                 icon: "/portfolio/icon/ico_web.png",
@@ -43,31 +43,14 @@ const projectData = [
 
     },
     {
-        id: "invoker",
-        icon: "/portfolio/projects/ico_invoker.png",
-        title: "Invoker Tactic",
-        description: "Tactical RPG",
-        image: "projects/invokertactic.png",
-        keyFeatures: [],
-        developmentTeam: "Solo Project",
-        technology: [],
-        buttons: [
-            {
-                icon: "/portfolio/icon/ico_youtube.png",
-                text: "Demo",
-                href: "https://youtu.be/Fv4muer8GAc",
-            },
-        ],
-    },
-    {
         id: "vou",
         icon: "/portfolio/projects/ico_vou.svg",
-        title: "VOU Game",
+        title: "VOU Game (Unity/C#/Node.js/NextJS)",
         description: "(Graduation Project) A Gamify Marketing Platform",
         image: "projects/vou.svg",
-        keyFeatures: [],
-        developmentTeam: "4 People",
-        technology: [],
+        keyFeatures: ["Online Multiplayer", "Mobile Client", "Web Client"],
+        developmentTeam: "4 Members",
+        technology: ["Node.js", "Unity", "Docker", "Linux", "Nginx", "MongoDB", "PostgreSQL"],
         buttons: [
             {
                 icon: "/portfolio/icon/ico_web.png",
@@ -77,14 +60,31 @@ const projectData = [
         ],
     },
     {
+        id: "modelMinosWorld",
+        icon: "/portfolio/projects/ico_minos.png",
+        title: "Minos World (Unity/C#)",
+        description: "Mobile Side-Scrolling Platformer",
+        image: "projects/minosworld.png",
+        keyFeatures: ["Firebase Analytics", "In-App Purchase", "AdMob Integration", "IronSource Integration"],
+        developmentTeam: "7 Members",
+        technology: ["Unity", "C#"],
+        buttons: [
+            {
+                icon: "/portfolio/icon/ico_android.png",
+                text: "Google Play",
+                link: "https://play.google.com/store/apps/details?id=com.marinobros.jump.run.world.adventure",
+            },
+        ],
+    },
+    {
         id: "modelTapMaster3D",
-        title: "Tap Master 3D",
+        title: "Tap Master 3D (Unity/C#)",
         icon: "/portfolio/projects/ico_tap.png",
         description: "Mobile Puzzle Game",
         image: "projects/tapmaster3d.png",
-        keyFeatures: [],
-        developmentTeam: "4 People",
-        technology: [],
+        keyFeatures: ["Firebase Analytics", "In-App Purchase", "AdMob Integration", "IronSource Integration"],
+        developmentTeam: "4 Members",
+        technology: ["Unity", "C#"],
         buttons: [
             {
                 icon: "/portfolio/icon/ico_android.png",
@@ -94,19 +94,63 @@ const projectData = [
         ],
     },
     {
-        id: "modelMinosWorld",
-        icon: "/portfolio/projects/ico_minos.png",
-        title: "Minos World",
-        description: "Mobile Side-Scrolling Platformer",
-        image: "projects/minosworld.png",
-        keyFeatures: [],
-        developmentTeam: "5+ People",
-        technology: [],
+        id: "frog",
+        icon: "/portfolio/projects/ico_frog.png",
+        title: "Crossing Road (C++)",
+        description: "Crossing Road Game written in C++ with OOP.",
+        image: "projects/snake.png",
+        keyFeatures: ["Save/Load", "Leaderboard", "Settings", "4 Different Game Modes", "Used Native C++ Graphics", "Local Multiplayer"],
+        developmentTeam: "3 Members",
+        technology: ["C", "C++", "OOP", "CMake"],
         buttons: [
             {
-                icon: "/portfolio/icon/ico_android.png",
-                text: "Google Play",
-                link: "https://play.google.com/store/apps/details?id=com.marinobros.jump.run.world.adventure",
+                icon: "/portfolio/icon/ico_youtube.png",
+                text: "Demo",
+                href: "https://www.youtube.com/watch?v=YcGgp-uGTGc",
+            },
+            {
+                icon: "/portfolio/icon/ico_github.png",
+                text: "Github",
+                href: "https://github.com/LightKod/OOP-Crossing-Road",
+            },
+        ],
+    },
+    {
+        id: "snake",
+        icon: "/portfolio/projects/ico_snake.png",
+        title: "Snake Game (C)",
+        description: "A Snake game written in C/C++.",
+        image: "projects/snake.png",
+        keyFeatures: ["Save/Load", "Leaderboard", "Settings", "4 Different Game Modes", "Used Native C Graphics"],
+        developmentTeam: "4 Members",
+        technology: ["C", "CMake"],
+        buttons: [
+            {
+                icon: "/portfolio/icon/ico_youtube.png",
+                text: "Demo",
+                href: "https://www.youtube.com/watch?v=roe3JsHokfA",
+            },
+            {
+                icon: "/portfolio/icon/ico_github.png",
+                text: "Github",
+                href: "https://github.com/LightKod/DoAnKTLT",
+            },
+        ],
+    },
+    {
+        id: "invoker",
+        icon: "/portfolio/projects/ico_invoker.png",
+        title: "Invoker Tactic (Unity/C#)",
+        description: "A Tactical RPG Game designed and developed by myself.",
+        image: "projects/invokertactic.png",
+        keyFeatures: ["Randomize Map", "20+ Units", "50+ Skills", "50+ Items", "10+ Maps"],
+        developmentTeam: "Solo Project",
+        technology: ["Unity", "C#", "Photoshop", "Asesprite", "FMod"],
+        buttons: [
+            {
+                icon: "/portfolio/icon/ico_youtube.png",
+                text: "Demo",
+                href: "https://youtu.be/Fv4muer8GAc",
             },
         ],
     },
